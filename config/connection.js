e8d974a46cb57d5d3e3e71e0a1f897720a028ec5
David Harris <davidharris006@gmail.com>
2019-06-02 20:04:20 -0700

    more Heroku code added

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
     password: "root",
     database: "burgers_db"
   });
+
+  if  (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'hacktheplanet',
+      database: 'todoagian_db'
+    })
+  }
   
   connection.connect(function(err) {
     if (err) {
